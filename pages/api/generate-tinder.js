8a4b4facf6dc6c5651f5ff228343c386dd149b78
Cilla Jelf <cilla@airlab.io>
2023-01-25 17:14:18 -0800

    Update generate-tinder.js

--- pages/api/generate-tinder.js
+++ pages/api/generate-tinder.js
@@ -28,13 +28,11 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(req.body.specific, req.body.measurable, req.body.achievable, req.body.relevant, req.body.timely, req.body.evaluation, req.body.readjust),
+      prompt: generatePrompt(req.body.specific, req.body.measurable, req.body.achievable, req.body.relevant, req.body.timely),
       temperature: 0.9,
       max_tokens: 700,
      
     });
-
-    // console.log('prompt---->', generatePrompt(req.body.achievement, req.body.team, req.body.startDate, req.body.endDate))
 
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -53,41 +51,25 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(specific, measurable,  achievable,  relevant, timely, evaluation, readjust) {
-return `Create a suggestion for a goal, using the SMARTER goal setting framework, based on the following:
+function generatePrompt(specific, measurable,  achievable,  relevant, timely) {
+return `Create a suggestion for a goal, using the SMART goal setting framework, based on the following:
 
-Specific: Increase website traffic
-Measurable: Using Google Analytics we will increase website traffic by 25%.
-Achievable: We will increase website traffic by 25% by creating 5 extra blog posts a week within the next three months.
-Relevant: This is connected to our company’s goal of increasing revenue by 20% in 2023.
-Timely: Q1 and Q2 2023
-Evaluation: We will evaluate the success of the objective by comparing the number of website visitors in Q1 and Q2 2023 to the same period in 2022, once per month.
-Readjust: If the objective is a success, consider extending the process for another 3 months with new targets. If the team feel overwhelmed, consider reducing the number of blog posts to 3 per week.
+Specific: Marketing team will increase website traffic by producing more content to drive organic traffic.
+Measurable: We will increase monthly visitors by 25%. Using Google Analytics we will measure the number of website visitors, baseline is currently 1000 visitors per month.
+Achievable: With our expansion of the marketing team we can create 5 extra blog posts a week within the next three months.
+Relevant: This is connected to our company’s goal of increasing revenue by 10% in 2023.
+Timely: Feb 1 2023, to June 30, 2023
 
-Suggestion: 
-Specific: Increase website traffic
-Measurable: Using Google Analytics we will increase website traffic by 25%.
-Achievable: We will increase website traffic by 25% by creating 5 extra blog posts a week within the next three months.
-Relevant: This is connected to our company’s goal of increasing revenue by 20% in 2023.
-Timely: Q1 and Q2 2023
-Evaluation: We will evaluate the success of the objective by comparing the number of website visitors in Q1 and Q2 2023 to the same period in 2022, once per month.
-Readjust: If the objective is a success, consider extending the process for another 3 months with new targets. If the team feel overwhelmed, consider reducing the number of blog posts to 3 per week.
-
-Goal: Increase website traffic by 25% by creating 5 extra blog posts a week, starting now and continue until 6/30/2023. 
-If the objective is a success, consider extending the process for another 3 months with new targets. 
-If the marketing team feel overwhelmed, consider reducing the number of blog posts to 3 per week.
-
+Suggestion Goal: We will increase organic traffic by 25% by the end of Q2. We will do this by creating 5 extra blog posts a week, starting now and continue until 6/30/2023. 
+ 
 
 Specific: ${specific}
 Measurable: ${measurable}
 Achievable: ${achievable}
-Timely: ${timely}
-Evaluation: ${evaluation}
 Relevant: ${relevant}
-Evaluation: ${evaluation}
-Readjust: ${readjust}
+Timely: ${timely}
 
-Suggestion:
+Suggestion Goal:
 `
 
 
