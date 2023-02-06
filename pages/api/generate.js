ad1cbc09c32cbba7016f1af7ce1dd88452c8a8f8
Cilla Jelf <cilla@airlab.io>
2023-02-06 09:39:01 -0800

    Error handling

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -39,18 +39,23 @@ export default async function (req, res) {
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
    
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          // message: 'An error occurred during your request.',
-  
-          message: 'xxx',      }
-      });
+    // if (error.response) {
+    //   console.error(error.response.status, error.response.data);
+    //   res.status(error.response.status).json(error.response.data);
+    // } 
+    if (response.status !== 200) {
+      throw new Error(`xxx Request failed with status ${response.status}`);
     }
+    
+    // else {
+    //   console.error(`Error with OpenAI API request: ${error.message}`);
+    //   res.status(500).json({
+    //     error: {
+    //       // message: 'An error occurred during your request.',
+  
+    //       message: 'xxx',      }
+    //   });
+    // }
   }
 }
 
